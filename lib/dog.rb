33f1962c08f5981631f3c0e5843b56693ba324c6
Samantha Cassity <cassityclaire@gmail.com>
2018-06-08 17:22:23 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # dog.rb
+class Dog
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+  def name 
+    @this_dogs_name
+end
+end
+  fido=Dog.new
+  fido.name= "fido"
+  puts "The first dog's name is #{fido.name}"
+  snoopy=Dog.new 
+  snoopy.name = "snoopy"
+  puts "The second dog's name is #{snoopy.name}"
+  lassie=Dog.new
+  lassie.name = "lassie"
+  puts "The third dog's name is #{lassie.name}"
