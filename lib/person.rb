b9654fd7e140bfda26966ce3ba95082179aa8deb
Samantha Cassity <cassityclaire@gmail.com>
2018-06-08 17:22:18 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,16 @@
 # person.rb
+class Person 
+  def person(person_name)
+    @this_persons_name = person_name
+  end
+  def name
+    @this_persons_name
+
+  end
+end
+adele_goldberg=Person.new 
+adele_goldberg.name= "Adele Goldberg"
+puts "This person's name is #{adele_goldberg.name}"
+alan_kay=Person.new
+alan_kay.name= "Alan Kay"
+puts "This person's name is #{alan_kay.name}"
